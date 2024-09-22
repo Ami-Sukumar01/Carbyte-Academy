dd3e3e9e58c250154e11d4d32434c1da85b7cd0b
Ami-Sukumar01 <AMI.SUKUMAR01@GMAIL.COM>
2024-09-22 11:55:03 +0200

    Merged specific files from 1274 into 1287

---
+++ app/spaces/page.tsx
@@ -0,0 +1,12 @@
+import PageHeader from "@/components/Header"
+import TestComponent from "@/components/TestComponent"
+
+
+export default function SpacePage() {
+    return (
+        <div>
+        <PageHeader/>
+
+        </div>
+    )
+}
---
+++ components/IconBox.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+
+type IconBoxProps = {
+  icon: ReactNode;
+};
+
+const IconBox = ({ icon }: IconBoxProps) => (
+  <div className='flex w-[52px] h-[52px] shrink-0 grow-0 items-center justify-center rounded-[30px] bg-white transition-shadow duration-300 ease-in-out hover:shadow-[6px_6px_0_black] '>
+    {icon}
+  </div>
+);
+
+IconBox.displayName = 'IconBox';
+export default IconBox;
+
