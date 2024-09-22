93b67d6397b2653e8c77b2151ef3e3cc6d679bc1
Ami-Sukumar01 <AMI.SUKUMAR01@GMAIL.COM>
2024-09-22 11:01:40 +0200

    added hover status to the header icon

--- components/IconBox.tsx
+++ components/IconBox.tsx
@@ -5,7 +5,7 @@ type IconBoxProps = {
 };
 
 const IconBox = ({ icon }: IconBoxProps) => (
-  <div className='flex w-[52px] h-[52px] shrink-0 grow-0 items-center justify-center rounded-[30px] bg-white '>
+  <div className='flex w-[52px] h-[52px] shrink-0 grow-0 items-center justify-center rounded-[30px] bg-white transition-shadow duration-300 ease-in-out hover:shadow-[6px_6px_0_black] '>
     {icon}
   </div>
 );
