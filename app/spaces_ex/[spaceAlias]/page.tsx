547b1d51f576737882d8c81ffcb6476bf1e7243f
adonisote <adonis.almagro@code.berlin>
2024-10-08 07:07:33 +0200

    Work in Progres Replacing EndPoint for SpacePage

--- app/spaces_ex/[spaceAlias]/page.tsx
+++ app/spaces_ex/[spaceAlias]/page.tsx
@@ -1,9 +1,22 @@
-// import { useParams } from "next/navigation";
+"use client"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
 
-export default async function SpacePage({ params }: { params: { spaceAlias: string } }) {
+
+//TO DO: DEFINE a Type once EndPoint Ready
+
+export default function SpacePage({ params }: { params: { spaceAlias: string } }) {
+  const router = useRouter()
   const spaceAlias = decodeURIComponent(params.spaceAlias)
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/spaces/${spaceAlias}`)
+
+
+  const res = fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/spaces/${spaceAlias}`) // Not ready to use
+
+  const handleRedirect = (spaceAlias: string) => {
+    router.push(`/spaces_ex/${encodeURIComponent(spaceAlias)}/r`)
+  }
+
 
 
 
@@ -11,6 +24,11 @@ export default async function SpacePage({ params }: { params: { spaceAlias: stri
     <div>
       <h1>Space Page</h1>
       <p>Alias: {spaceAlias}</p>
+      <button
+        className="border p-4"
+        onClick={() => handleRedirect(spaceAlias)}
+      >Resources</button>
+      <li>End point for this is not ready yet. Check Space Resources</li>
     </div>
   )
 }
