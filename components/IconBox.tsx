00aa3ca22432d2905c282eeeee3c7052defb92bd
Ami-Sukumar01 <AMI.SUKUMAR01@GMAIL.COM>
2024-10-10 11:45:19 +0200

    changing the icon box from rounded to square as per design change

--- components/IconBox.tsx
+++ components/IconBox.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from 'react';
+import { Heart, Star } from 'lucide-react';
 
 type IconBoxProps = {
   icon: ReactNode;
 };
 
 const IconBox = ({ icon }: IconBoxProps) => (
-  <div className='flex w-[52px] h-[52px] shrink-0 grow-0 items-center justify-center rounded-[30px] bg-white transition-shadow duration-300 ease-in-out hover:shadow-[6px_6px_0_black]'>
+  <div className='flex w-[52px] h-[52px] shrink-0 grow-0 items-center justify-center bg-white transition-shadow duration-300 ease-in-out hover:shadow-[6px_6px_0_black]'>
     {icon}
   </div>
 );
