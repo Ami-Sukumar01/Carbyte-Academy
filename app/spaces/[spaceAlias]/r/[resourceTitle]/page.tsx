eb29ecf1cef153597dd2e2321cff979bfa17dad7
Ami-Sukumar01 <AMI.SUKUMAR01@GMAIL.COM>
2024-11-02 18:56:39 +0100

    specific resource page

--- app/spaces/[spaceAlias]/r/[resourceTitle]/page.tsx
+++ app/spaces/[spaceAlias]/r/[resourceTitle]/page.tsx
@@ -5,7 +5,7 @@ export default async function ResourcePage({ params }: { params: { spaceAlias: s
 
   return (
     <>
-      <p className="text-2xl hover:bg-green-400" >Specific Resource Page</p>
+      {/* <p className="text-2xl hover:bg-green-400" >Specific Resource Page</p> */}
       <ResourceDetail spaceAlias={params.spaceAlias} resourceTitle={params.resourceTitle} />
     </>
   )
