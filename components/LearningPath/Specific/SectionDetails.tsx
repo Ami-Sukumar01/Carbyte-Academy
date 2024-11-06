b64f42c4a8928f295821d39223aa0fecc4eb0d92
adonisote <adonis.almagro@code.berlin>
2024-11-06 21:03:40 +0100

    fix small bug

--- components/LearningPath/Specific/SectionDetails.tsx
+++ components/LearningPath/Specific/SectionDetails.tsx
@@ -10,13 +10,13 @@ export function SecctionDetails({ spaceAlias, section }: { spaceAlias: string, s
       <p>{section.title}</p>
       <p>{section.description}</p>
       <p className="text-center">Resources:</p>
-      <p>{section.resourceSections?.map((sectionResource) => (
+      <div>{section.resourceSections?.map((sectionResource) => (
         <div key={sectionResource.sectionId}>
           <Link href={`/spaces/${spaceAlias}/r/${sectionResource.resource.title}`} className="hover:bg-slate-500">
             <p key={sectionResource.sectionId}>{sectionResource?.resource.title}</p>
           </Link>
         </div>
-      ))}</p>
+      ))}</div>
     </div>
   )
 }
