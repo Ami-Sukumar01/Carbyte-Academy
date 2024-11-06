74262157a0ce95745d20c50b5a1a025c54e03f8e
adonisote <adonis.almagro@code.berlin>
2024-11-06 22:20:30 +0100

    group between normal and special spaces page and learninpath page

--- app/(app)/(normal)/spaces/layout.tsx
+++ app/(app)/(normal)/spaces/layout.tsx
@@ -1,6 +1,6 @@
 import PageHeader from "@/components/Header";
-import Navbar from "../../components/NavBar";
-import SideBar from "../../components/SideBar";
+import Navbar from "@/components/NavBar";
+import Sidebar from "@/components/SideBar";
 
 export default function SpacesLayout({
   children,
