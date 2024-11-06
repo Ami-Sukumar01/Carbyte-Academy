763b255f0eec912d7c4f4941c00f7736ee9c9989
adonisote <adonis.almagro@code.berlin>
2024-11-06 21:38:26 +0100

    move resource page and add resource to grouped route

---
+++ app/(app)/spaces/[spaceAlias]/r/add/layout.tsx
@@ -0,0 +1,14 @@
+export default function AddResourceLayout(
+  {
+    children,
+  }: {
+    children: React.ReactNode;
+  }
+) {
+
+  return (
+    <section>
+      {children}
+    </section>
+  )
+}
---
+++ app/(app)/spaces/[spaceAlias]/r/add/page.tsx
@@ -0,0 +1,5 @@
+export default async function AddResourcePage() {
+  return (
+    <div>Add a resource</div>
+  )
+}
