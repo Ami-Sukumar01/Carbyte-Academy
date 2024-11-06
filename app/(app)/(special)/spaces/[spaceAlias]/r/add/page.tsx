bfbd6ab60efba62d5be976696ed18f1504e90d1c
adonisote <adonis.almagro@code.berlin>
2024-11-06 22:44:10 +0100

    add resource in progres

--- app/(app)/(special)/spaces/[spaceAlias]/r/add/page.tsx
+++ app/(app)/(special)/spaces/[spaceAlias]/r/add/page.tsx
@@ -1,5 +1,19 @@
-export default async function AddResourcePage() {
+import { notFound } from "next/navigation"
+import AddResourceForm from "@/components/Resources/AddResource/AddResourceForm"
+import { fetchSpaceId } from "@/lib/data"
+
+export default async function AddResource({ params }: { params: { spaceAlias: string } }) {
+  const spaceId = await fetchSpaceId(params.spaceAlias) //Get spaceId
+  if (!spaceId) {
+    notFound() //404 if no spaceId found
+  }
+
+  //Get Us
+  const userId = "55fc2914-20e2-41a9-9478-222896d2365a" //TODO: User LOGIN -> USER ID in popscle Fetch from Popscle Users
   return (
-    <div>Add a resource</div>
+    <>
+      <p>Add a resource</p>
+      <AddResourceForm spaceAlias={params.spaceAlias} spaceId={spaceId} userId={userId} />
+    </>
   )
 }
