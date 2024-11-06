325a8b7097720f4071eb5737cfecb6d238f7ae17
adonisote <adonis.almagro@code.berlin>
2024-11-06 21:41:10 +0100

    add group for landing page

---
+++ app/(landing-page)/home/layout.tsx
@@ -0,0 +1,12 @@
+export default function LandingPageLayout({ children }: { children: React.ReactNode }) {
+
+  return (
+
+    // <html>
+    <main className="bg-yellow-200 w-full h-full">
+      {children}
+    </main>
+
+
+  )
+}
---
+++ app/(landing-page)/home/page.tsx
@@ -0,0 +1,5 @@
+export default async function HomePage() {
+  return (
+    <div className="w-full h-full">Home Popscle</div>
+  )
+}
