86cd576a4e968ca67ba5c6fb29d0007360f19c9c
adonisote <adonis.almagro@code.berlin>
2024-11-17 14:43:14 +0100

    Fix Bug: One Source of True Database URL

--- app/(landing-page)/home/page.tsx
+++ app/(landing-page)/home/page.tsx
@@ -1,5 +1,8 @@
 export default async function HomePage() {
+  const environement = process.env.NODE_ENV
   return (
-    <div className="w-full h-full">Home Popscle</div>
+    <div className="w-full h-full">Home Popscle
+      <div>Runing on: {environement}</div>
+    </div>
   )
 }
